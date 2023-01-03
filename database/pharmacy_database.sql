f68d9fd2bcee0a0f764bc668b21d3ebbb980c1e4
kundananji <michaelsinkolongo@gmail.com>
2023-01-03 12:51:23 +0200

    Initial commit

---
+++ database/pharmacy_database.sql
@@ -0,0 +1,322 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 03, 2023 at 11:49 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pharmacy`
+--
+CREATE DATABASE IF NOT EXISTS `pharmacy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `pharmacy`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_credentials`
+--
+
+CREATE TABLE `admin_credentials` (
+  `USERNAME` varchar(50) COLLATE utf16_bin NOT NULL,
+  `PASSWORD` varchar(50) COLLATE utf16_bin NOT NULL,
+  `ADDRESS` varchar(255) COLLATE utf16_bin DEFAULT NULL,
+  `EMAIL` varchar(150) COLLATE utf16_bin DEFAULT NULL,
+  `CONTACT_NUMBER` varchar(20) COLLATE utf16_bin DEFAULT NULL,
+  `PHARMACY_NAME` varchar(100) COLLATE utf16_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `admin_credentials`
+--
+
+INSERT INTO `admin_credentials` (`USERNAME`, `PASSWORD`, `ADDRESS`, `EMAIL`, `CONTACT_NUMBER`, `PHARMACY_NAME`) VALUES
+('admin', 'admin123', 'Plot No. 24, Riverside Extension, KITWE', 'info@pharmacy.com', '095323124', 'Linked Pharmacy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(20) COLLATE utf16_bin NOT NULL,
+  `CONTACT_NUMBER` varchar(10) COLLATE utf16_bin NOT NULL,
+  `ADDRESS` varchar(100) COLLATE utf16_bin NOT NULL,
+  `DOCTOR_NAME` varchar(20) COLLATE utf16_bin NOT NULL,
+  `DOCTOR_ADDRESS` varchar(100) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`ID`, `NAME`, `CONTACT_NUMBER`, `ADDRESS`, `DOCTOR_NAME`, `DOCTOR_ADDRESS`) VALUES
+(4, 'Kiran Suthar', '1234567690', 'Andheri East', 'Anshari', 'Andheri East'),
+(6, 'Aditya', '7365687269', 'Virar West', 'Xyz', 'Virar West'),
+(11, 'Shivam Tiwari', '6862369896', 'Dadar West', 'Dr Kapoor', 'Dadar East'),
+(13, 'Varsha Suthar', '7622369694', 'Rani Station', 'Dr Ramesh', 'Rani Station'),
+(14, 'Prakash Bhattarai', '9802851472', 'Pokhara-16, Dhikidada', 'Hari Bahadur', 'Matepani-12'),
+(15, 'Michael Sinkolongo', '0979474203', 'Plot No. 1, Julia Chikamoneka, Kitwe, Zambia', 'Teddy Kajimoto', 'Plot No. 2, Julia Chikamoneka, Kitwe, Zambia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoices`
+--
+
+CREATE TABLE `invoices` (
+  `INVOICE_ID` int(11) NOT NULL,
+  `NET_TOTAL` double NOT NULL DEFAULT 0,
+  `INVOICE_DATE` date NOT NULL DEFAULT current_timestamp(),
+  `CUSTOMER_ID` int(11) NOT NULL,
+  `TOTAL_AMOUNT` double NOT NULL,
+  `TOTAL_DISCOUNT` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `invoices`
+--
+
+INSERT INTO `invoices` (`INVOICE_ID`, `NET_TOTAL`, `INVOICE_DATE`, `CUSTOMER_ID`, `TOTAL_AMOUNT`, `TOTAL_DISCOUNT`) VALUES
+(2, 2626, '2021-10-19', 6, 2626, 0),
+(3, 5282, '2023-01-03', 15, 5282, 0),
+(4, 30, '2023-01-03', 15, 30, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicines`
+--
+
+CREATE TABLE `medicines` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `PACKING` varchar(20) COLLATE utf16_bin NOT NULL,
+  `GENERIC_NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `SUPPLIER_NAME` varchar(100) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `medicines`
+--
+
+INSERT INTO `medicines` (`ID`, `NAME`, `PACKING`, `GENERIC_NAME`, `SUPPLIER_NAME`) VALUES
+(1, 'Nicip Plus', '10tab', 'Paracetamole', 'BDPL PHARMA'),
+(2, 'Crosin', '10tab', 'Hdsgvkvajkcbja', 'Kiran Pharma'),
+(4, 'Dolo 650', '15tab', 'paracetamole', 'BDPL PHARMA'),
+(5, 'Gelusil', '10tab', 'mint fla', 'Desai Pharma'),
+(6, 'Pandol', 'BOTTLE', 'Panadol', 'Kundananji Creations Limited');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicines_stock`
+--
+
+CREATE TABLE `medicines_stock` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `BATCH_ID` varchar(20) COLLATE utf16_bin NOT NULL,
+  `EXPIRY_DATE` varchar(10) COLLATE utf16_bin NOT NULL,
+  `QUANTITY` int(11) NOT NULL,
+  `MRP` double NOT NULL,
+  `RATE` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `medicines_stock`
+--
+
+INSERT INTO `medicines_stock` (`ID`, `NAME`, `BATCH_ID`, `EXPIRY_DATE`, `QUANTITY`, `MRP`, `RATE`) VALUES
+(1, 'Crosin', 'CROS12', '12/34', 0, 2626, 26),
+(2, 'Gelusil', 'G327', '12/42', 0, 15, 12),
+(3, 'Dolo 650', 'DOLO327', '01/23', 1, 30, 24),
+(4, 'Nicip Plus', 'NI325', '05/22', 3, 32.65, 28);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchases`
+--
+
+CREATE TABLE `purchases` (
+  `SUPPLIER_NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `INVOICE_NUMBER` int(11) NOT NULL,
+  `VOUCHER_NUMBER` int(11) NOT NULL,
+  `PURCHASE_DATE` varchar(10) COLLATE utf16_bin NOT NULL,
+  `TOTAL_AMOUNT` double NOT NULL,
+  `PAYMENT_STATUS` varchar(20) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `purchases`
+--
+
+INSERT INTO `purchases` (`SUPPLIER_NAME`, `INVOICE_NUMBER`, `VOUCHER_NUMBER`, `PURCHASE_DATE`, `TOTAL_AMOUNT`, `PAYMENT_STATUS`) VALUES
+('Kundananji Creations Limited', 11, 1, '2023-01-03', 180, 'PAID');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `CUSTOMER_ID` int(11) NOT NULL,
+  `INVOICE_NUMBER` varchar(100) DEFAULT NULL,
+  `MEDICINE_NAME` varchar(100) DEFAULT NULL,
+  `BATCH_ID` varchar(100) DEFAULT NULL,
+  `EXPIRY_DATE` varchar(20) DEFAULT NULL,
+  `QUANTITY` int(11) DEFAULT NULL,
+  `MRP` double DEFAULT NULL,
+  `DISCOUNT` decimal(10,2) DEFAULT NULL,
+  `TOTAL` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`CUSTOMER_ID`, `INVOICE_NUMBER`, `MEDICINE_NAME`, `BATCH_ID`, `EXPIRY_DATE`, `QUANTITY`, `MRP`, `DISCOUNT`, `TOTAL`) VALUES
+(15, '4', 'Dolo 650', 'DOLO327', '01/23', 1, 30, '0.00', '30.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(100) COLLATE utf16_bin NOT NULL,
+  `EMAIL` varchar(100) COLLATE utf16_bin NOT NULL,
+  `CONTACT_NUMBER` varchar(10) COLLATE utf16_bin NOT NULL,
+  `ADDRESS` varchar(100) COLLATE utf16_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`ID`, `NAME`, `EMAIL`, `CONTACT_NUMBER`, `ADDRESS`) VALUES
+(1, 'Desai Pharma', 'desai@gmail.com', '9948724242', 'Mahim East'),
+(2, 'BDPL PHARMA', 'bdpl@gmail.com', '8645632963', 'Santacruz West'),
+(9, 'Kiran Pharma', 'kiranpharma@gmail.com', '7638683637', 'Andheri East'),
+(10, 'Rsrnrnrndnn', 'ydj', '3737355538', '3fndfndfndndfnfdndfn'),
+(11, 'Dfnsfndfndf', 'fnsn', '5475734385', 'Ndnss4yrhrhdhrdhrh'),
+(12, 'SS Distributors', 'ssdis@gamil.com', '3867868752', 'Matunga West'),
+(13, 'Avceve', 'ehh', '3466626226', 'Eteh266266262'),
+(14, 'Hrshrhrjher', 'dzgdg', '4636347335', 'Rhrswjrnswjn'),
+(15, 'Hmrxfmgtmt', 'trmtrm gm tr', '6553838835', '38ejtdjtdxetjdt'),
+(20, 'Dtdxtkmtdshrrhhsrjrs', 'trmtrm gm tr', '6553838835', '38ejtdjtdxetjdt'),
+(23, 'Fndn', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
+(24, 'Fndnbrwh', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
+(25, 'Jnentjrtj', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
+(26, 'Jerthjrtjtjr', 'nena ena', '3462462642', 'Ebsbsdbsdndsnsdfns'),
+(30, 'Kundananji Creations Limited', 'info@kundananjicreations.com', '9794747203', 'MC 28PHI CHAINAMA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_credentials`
+--
+ALTER TABLE `admin_credentials`
+  ADD PRIMARY KEY (`USERNAME`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `invoices`
+--
+ALTER TABLE `invoices`
+  ADD PRIMARY KEY (`INVOICE_ID`);
+
+--
+-- Indexes for table `medicines`
+--
+ALTER TABLE `medicines`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `medicines_stock`
+--
+ALTER TABLE `medicines_stock`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `BATCH_ID` (`BATCH_ID`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`VOUCHER_NUMBER`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `invoices`
+--
+ALTER TABLE `invoices`
+  MODIFY `INVOICE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `medicines`
+--
+ALTER TABLE `medicines`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `medicines_stock`
+--
+ALTER TABLE `medicines_stock`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `VOUCHER_NUMBER` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
